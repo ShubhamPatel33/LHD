f97a4410c8ba25c4b148275da0083d9ab593bf0e
aakiliqbal <aakiliqbal645@gmail.com>
2021-01-11 20:48:36 +0530

    added hello world in C#

---
+++ Hello world in C#/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("LHD Rocks");
+            var name = "Aakil Iqbal";
+            Console.WriteLine($"I am {name}");
+
+        }
+    }
+}
